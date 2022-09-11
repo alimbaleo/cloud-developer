bcaee7892d32ce38c287d5837a05ecd81f948a78
Alimba Chukwuemeka <alimbaleo@gmail.com>
2022-09-11 15:18:38 +0100

    CHANGES TO DELOY TO EBS

---
+++ course-02/exercises/udacity-c2-restapi/.profile
@@ -0,0 +1,7 @@
+export POSTGRES_USERNAME=hellopostgres;
+export POSTGRES_PASSWORD=hellopostgres;
+export POSTGRES_DATABASE=hellopostgres;
+export POSTGRES_HOST=hellopostgres.ctfqokbwtlld.us-east-1.rds.amazonaws.com;
+export DATABASE_DIALECT=postgres;
+export AWS_REGION=us-east-1;
+export S3_BUCKET_NAME=udagram-aleo-dev;
--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "username": process.env.POSTGRES_USERNAME,// "hellopostgres",
+    "password": process.env.POSTGRES_PASSWORD,//"hellopostgres",
+    "database": process.env.POSTGRES_DATABASE,//"hellopostgres",
+    "host": process.env.POSTGRES_HOST,//"hellopostgres.ctfqokbwtlld.us-east-1.rds.amazonaws.com",
+    "dialect": process.env.DATABASE_DIALECT,//"postgres",
+    "aws_region": process.env.AWS_REGION,//"us-east-1",
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.S3_BUCKET_NAME,//"udagram-aleo-dev"
   },
   "jwt": {
     "secret": " "
